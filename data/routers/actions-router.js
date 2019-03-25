be5a7ee960502123c560d88dc7b17cafc9ac389e
Derek Jones <derek@derekjones.io>
2019-03-25 00:05:18 -0400

    Finish adding end points

--- data/routers/actions-router.js
+++ data/routers/actions-router.js
@@ -23,4 +23,44 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  const action = req.body;
+
+  if (action.project_id && action.description && action.notes) {
+    try {
+      const actions = await actionDb.insert(action);
+      res.status(201).json(actions);
+    } catch (error) {
+      res.status(500).json({ error: 'error adding action' });
+    }
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const action = req.body;
+
+  if (action.project_id && action.description && action.notes) {
+    try {
+      const actions = await actionDb.update(id, action);
+      res.status(200).json(actions);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  } else {
+    res.status(500).json({ error: 'error updating action' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const actions = actionDb.remove(id);
+    res.status(200).json(actions);
+  } catch (error) {
+    res.status(500).json({ error: 'error deleting action' });
+  }
+});
+
 module.exports = router;
--- data/routers/projects-router.js
+++ data/routers/projects-router.js
@@ -23,4 +23,46 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  const { name, description, completed } = req.body;
+
+  if ((name, description, completed)) {
+    try {
+      const projects = await projectsDb.insert(name, description, completed);
+      res.status(201).json(projects);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  } else {
+    res.status(500).json({ error: 'error adding project' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, description, completed } = req.body;
+
+  if ((name, description, completed)) {
+    try {
+      const projects = await projectsDb.update(id);
+      res.status(201).json(projects);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  } else {
+    res.status(500).json({ error: 'error updating project' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const projects = await projectsDb.remove(id);
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json({ error: 'error deleting project' });
+  }
+});
+
 module.exports = router;
