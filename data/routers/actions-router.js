22308b5f980d1090bbe1a5fb5beeaaa7184ba3b5
Derek Jones <derek@derekjones.io>
2019-03-24 23:37:34 -0400

    .get() by root and id set up for actions and projects

--- data/routers/actions-router.js
+++ data/routers/actions-router.js
@@ -3,4 +3,24 @@ const router = express.Router();
 
 const actionDb = require('../helpers/actionModel');
 
+router.get('/', async (req, res) => {
+  try {
+    const actions = await actionDb.get();
+    res.status(200).json(actions);
+  } catch (error) {
+    res.status(500).json({ error: 'error getting actions' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const actions = await actionDb.get(id);
+    res.status(200).json(actions);
+  } catch (error) {
+    res.status(500).json({ error: 'error getting action by id' });
+  }
+});
+
 module.exports = router;
--- data/routers/projects-router.js
+++ data/routers/projects-router.js
@@ -3,4 +3,24 @@ const router = express.Router();
 
 const projectDb = require('../helpers/projectModel');
 
+router.get('/', async (req, res) => {
+  try {
+    const projects = await projectDb.get();
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json({ error: 'error getting projects' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const projects = await projectDb.get(id);
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json({ error: 'error getting project by id' });
+  }
+});
+
 module.exports = router;
